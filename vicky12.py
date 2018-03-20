fa778e1bd0eda7b512b056fab88c2eed69809691
vickyjohn <36730720+vickyjohn@users.noreply.github.com>
2018-03-20 16:33:18 +0530

    Update vicky12.py

--- vicky12.py
+++ vicky12.py
@@ -1,11 +1,11 @@
 a=int(input("Enter number:"))
-temp=a
+b=a
 rev=0
 while(a>0):
     dig=a%10
     rev=rev*10+dig
     a=a//10
-if(temp==rev):
+if(b==rev):
     print("The number is a palindrome!")
 else:
     print("The number isn't a palindrome!")
